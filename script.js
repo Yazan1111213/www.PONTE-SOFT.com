e3c6c39db7612ae9623a839961b46d28a36820c8
PONTE.com <y77871735@gmail.com>
2024-06-03 02:25:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function scrollToelement(elementSelector, instance = 0){
+    
+    const elements = document.quarySelectorAll(elementSelector);
+
+    if(elements.length > instance){
+        elements[instance].scrollIntoView({ behavior: 'smooth});
+    }
+}
+
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+    link1.addEventListener('click'), () =>{
+        scrollToElement(.header);
+    };
+
+      link2.addEventListener('click'), () =>{
+        scrollToElement(.header);
+    };
+
+      link3.addEventListener('click'), () =>{
+        scrollToElement(.column);
+    };
+    
